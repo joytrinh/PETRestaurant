c72ed37f7c66a6962271d923909266e6b3c3eb96
Joy Trinh <mongtuyen149@gmail.com>
2019-02-24 11:28:48 +0700

    Create PET restaurant_ViewVersion

---
+++ 1788263_NhaHangPET/2_XuLy/MoHinhDoiTuong.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class XL_KhachThamQuan
+{
+    public string MaSo = "KhachThamQuan", HoTen = "KhachThamQuan";
+}
